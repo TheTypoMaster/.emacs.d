f526a414980ff547930e34e5958bb7127d5607e2
Nicholas Funnell <nfunnell@twilio.com>
2015-08-31 16:58:14 -0700

    `date`

--- init.el
+++ init.el
@@ -122,6 +122,9 @@ point reaches the beginning or end of the buffer, stop there."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" default)))
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
